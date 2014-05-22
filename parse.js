da7dcccf6b5174e02c671ea63100a5cb50f78354
James Halliday <mail@substack.net>
2014-05-23 00:13:14 +0200

    osmdata

--- parse.js
+++ parse.js
@@ -6,6 +6,7 @@ var parsers = require('./lib/parsers.js');
 var mode = 'size';
 var waiting = 4;
 var prev;
+var header;
 
 fs.createReadStream('/home/substack/osm')
     .pipe(through(write))
@@ -23,14 +24,14 @@ function write (buf, enc, next) {
     
     if (mode === 'size') {
         var len = buf.readUInt32BE(0);
-        console.log('SIZE', len);
+//console.log('SIZE', len);
         mode = 'header';
         waiting = len;
         write(buf.slice(4), enc, next);
     }
     else if (mode === 'header') {
-        var header = parsers.header.decode(buf.slice(0, waiting));
-        console.log('HEADER', header);
+        header = parsers.header.decode(buf.slice(0, waiting));
+//console.log('HEADER', header);
         mode = 'blob';
         var nbuf = buf.slice(waiting);
         waiting = header.datasize;
@@ -38,21 +39,28 @@ function write (buf, enc, next) {
     }
     else if (mode === 'blob') {
         var blob = parsers.blob.decode(buf.slice(0, waiting));
-        console.log('BLOB', blob);
+//console.log('BLOB', header.type, blob);
         
+        var h = header;
         zlib.inflate(blob.zlib_data, function (err, data) {
-            console.log(err, data);
-            var osm = parsers.osmheader.decode(data);
-            console.log('OSM', osm);
+            console.log(err, data, h);
+            if (h.type === 'OSMHeader') {
+                var osmh = parsers.osmheader.decode(data);
+                console.log('OSM HEADER', osmh);
+            }
+            else if (h.type === 'OSMData') {
+                var osmd = parsers.osmdata.decode(data);
+                console.log('OSM DATA', osmd);
+            }
         });
         
         mode = 'size';
+        var nbuf = buf.slice(waiting);
         waiting = 4;
-        write(buf.slice(waiting), enc, next);
+        write(nbuf, enc, next);
     }
 }
 
-;
 //var b = blob.decode(buf.slice(4+slice.length, 4+slice.length+115));
 //zlib.inflate(b.zlib_data, function(err, data) {
 //  console.log(osmheader.decode(data))
