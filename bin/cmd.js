24392435cd2d4b23e6c2c6d1c1171fc93c46434c
James Halliday <mail@substack.net>
2014-05-31 15:51:27 -0700

    comma

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,7 +5,7 @@ var http = require('http');
 var ecstatic = require('ecstatic');
 var through = require('through2');
 
-var file = __dirname + '/../data/planet-140514.osm.pbf.torrent';
+var file = __dirname + '/../data/planet-140528.osm.pbf.torrent';
 var torrentFile = fs.readFileSync(file);
 
 var parseOSM = require('osm-pbf-parser');
@@ -27,8 +27,6 @@ var sock = shoe(function onready (stream) {
     if (!ready) return engine.once('ready', onready);
     
     var rs = engine.files[0].createReadStream();
-    console.log(engine.files);
-    //var rs = fs.createReadStream('/home/substack/osm');
     var topts = { highWaterMark: 16 };
     rs.pipe(parseOSM()).pipe(through.obj(topts, function (row, enc, next) {
         this.push(JSON.stringify(row) + '\n');
--- bin/generate.js
+++ bin/generate.js
@@ -5,26 +5,29 @@ var through = require('through2');
 var file = __dirname + '/../data/planet-140528.osm.pbf.torrent';
 var torrentFile = fs.readFileSync(file);
 var geohash = require('geohasher');
-
 var parseOSM = require('osm-pbf-parser');
+
+var level = require('level');
+var db = level(__dirname + '/../data/hashes.db');
+var offset = parseInt(process.argv[2] || 0);
 
 var torrentStream = require('torrent-stream');
 var engine = torrentStream(torrentFile);
 engine.once('ready', function () {
-    var rs = engine.files[0].createReadStream();
+    var rs = engine.files[0].createReadStream({ start: offset });
     var topts = { highWaterMark: 16 };
     var osm = parseOSM();
     var start = 0;
     rs.pipe(osm).pipe(through.obj(topts, function (row, enc, next) {
         //console.log(JSON.stringify(row) + '\n');
         
-        console.log(row.type, start, osm._offset);
+        console.log(row.type, start + offset, osm._offset + offset);
         start = osm._offset;
         
         if (row.type === 'nodes') {
             row.nodes.forEach(function (r) {
                 var h = geohash.encode(r.lat, r.lon);
-                //console.log(r.id, h);
+                db.put(h, r.id);
             });
         }
         next();
