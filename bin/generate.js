75fbd40181527a8fe9c3b485bf57fe54c6dea7ee
James Halliday <mail@substack.net>
2014-06-07 18:01:26 -0700

    store the entire reference in leveldb

--- bin/generate.js
+++ bin/generate.js
@@ -8,26 +8,24 @@ var geohash = require('geohasher');
 var parseOSM = require('osm-pbf-parser');
 
 var level = require('level');
-var db = level(__dirname + '/../data/hashes.db');
+var db = level(__dirname + '/../data/hashes.db', { valueEncoding: 'json' });
 var offset = parseInt(process.argv[2] || 0);
 
 var torrentStream = require('torrent-stream');
-var engine = torrentStream(torrentFile);
+var engine = torrentStream(torrentFile, { tmp: __dirname + '/../data' });
 engine.once('ready', function () {
     var rs = engine.files[0].createReadStream({ start: offset });
     var topts = { highWaterMark: 16 };
     var osm = parseOSM();
     var start = 0;
     rs.pipe(osm).pipe(through.obj(topts, function (row, enc, next) {
-        //console.log(JSON.stringify(row) + '\n');
-        
         console.log(row.type, start + offset, osm._offset + offset);
         start = osm._offset;
         
         if (row.type === 'nodes') {
             row.nodes.forEach(function (r) {
                 var h = geohash.encode(r.lat, r.lon);
-                db.put(h, r.id);
+                db.put(h, r);
             });
         }
         next();
