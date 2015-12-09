d271c496e543d7fb8736f9c2552b5df3a54c9030
James Halliday <mail@substack.net>
2015-12-08 18:48:12 -0800

    population script runs without running out of memory

--- bin/index.js
+++ bin/index.js
@@ -5,12 +5,12 @@ var path = require('path')
 var through = require('through2')
 var ecef = require('geodetic-to-ecef')
 
-//var file = path.join(process.env.HOME, 'data/osm/alexandria_egypt.osm.pbf')
-var file = path.join(process.env.HOME, 'data/osm/auckland.pbf')
+var file = path.join(process.env.HOME, 'data/osm/alexandria_egypt.osm.pbf')
+//var file = path.join(process.env.HOME, 'data/osm/auckland.pbf')
 var OSM = require('osm-pbf-parser')
 
 var level = require('level')
-var ndb = level('/tmp/peerdb/nodes.db')
+var ndb = level('/tmp/peerdb/nodes.db', { valueEncoding: 'binary' })
 var wdb = level('/tmp/peerdb/ways.db', { valueEncoding: 'json' })
 
 var fdstore = require('fd-chunk-store')
@@ -28,34 +28,36 @@ fs.createReadStream(file)
   .pipe(osm)
   .pipe(through.obj(write))
 
-var tmp = new Buffer(8)
 var EventEmitter = require('events').EventEmitter
 var em = new EventEmitter
-var pending = 0
 
 function write (items, enc, next) {
+  var pending = 0
+  console.log(items.length)
   items.forEach(function (item) {
     if (item.type === 'node') {
-      tmp.writeFloatBE(item.lat, 0)
-      tmp.writeFloatBE(item.lon, 4)
-      ndb.put(String(item.id), tmp, function (err) {
+      pending++
+      var buf = new Buffer(8)
+      buf.writeFloatBE(item.lat, 0)
+      buf.writeFloatBE(item.lon, 4)
+      ndb.put(String(item.id), buf, function (err) {
         if (err) console.error(err)
-        else if (--pending === 0) em.emit('ready')
+        else if (--pending === 0) next()
       })
-    } else if (item.type === 'way' && pending === 0) {
-      way(item)
     } else if (item.type === 'way') {
-      em.once('ready', function () { way(item) })
+      way(item)
     }
   })
-  next()
+  if (pending === 0) next()
 
   function way (item) {
+    pending++
     wdb.put(String(item.id), item.tags, function (err) {
       if (err) console.error(err)
+      else if (--pending === 0) next()
     })
-
     item.refs.forEach(function (r) {
+      pending++
       ndb.get(String(r), function (err, buf) {
         if (err) return console.error(err)
         var lat = buf.readFloatBE(0)
@@ -63,6 +65,7 @@ function write (items, enc, next) {
         var xyz = ecef(lat, lon, 0)
         kdb.insert(xyz, item.id, function (err) {
           if (err) console.error(err)
+          else if (--pending === 0) next()
         })
       })
     })
