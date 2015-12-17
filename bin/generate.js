30851c9596d90beb71627933ca7af8427218f637
James Halliday <mail@substack.net>
2015-12-16 17:00:58 -0800

    indexes the alexandria extract in 2m30s

--- bin/generate.js
+++ bin/generate.js
@@ -10,76 +10,113 @@ var kdbtree = require('kdb-tree-store')
 var fdstore = require('fd-chunk-store')
 var level = require('level')
 
+var NODES = 1, WAYS = 2, RELATIONS = 3
+
 module.exports = function (osmfile, opts) {
   var workdir = opts.workdir || '/tmp/peermaps-workdir-' + Math.random()
   console.log(workdir)
 
   mkdirp(workdir, function (err) {
-    var osm = pipeline()
-    var index = 0
-    var limit = 200 * 1000
+    if (err) console.error(err)
+    process(workdir)
+  })
 
-    /*
+  function process (workdir) {
+    var db = level(path.join(workdir, 'db'), { valueEncoding: 'binary' })
+    var osm = OSM()
+    var limit = 200 * 1000
     var kdbfile = path.join(workdir, 'kdb')
     var kdb = kdbtree({
       types: [ 'float32', 'float32', 'float32', 'buffer[24]' ],
       size: 4096,
       store: fdstore(4096, kdbfile)
     })
-    */
-    var db = level(path.join(workdir, 'db'))
+    var mode = NODES
 
     fs.createReadStream(osmfile, opts)
       .pipe(osm)
       .pipe(through.obj(write))
 
     function write (items, enc, next) {
-      var offset = osm.offsets[index++]
+      if (mode === WAYS) return writeWay(items, enc, next)
+      if (mode === RELATIONS) return next()
 
       ;(function store (offset) {
         if (offset >= items.length) return next()
-        var ops = items.slice(offset, offset+limit)
-          .map(map).filter(Boolean)
-        if (ops.length === 0) return store(offset+limit)
-        db.batch(ops, function (err) {
+
+        var ops = []
+        for (var i = offset; i < items.length && ops.length < limit; i++) {
+          if (items[i].type === 'node') {
+            ops.push(nodeRecord(items[i]))
+          } else if (items[i].type === 'way') {
+            mode = WAYS
+            return writeWay(i === 0 ? items : items.slice(i), enc, next)
+          }
+        }
+        if (ops.length) db.batch(ops, function (err) {
           if (err) console.error(err)
           store(offset+limit)
         })
       })(0)
-
-      function map (item) {
-        if (item.type === 'node') {
-          var xyz = ecef(item.lat, item.lon, 0)
-          var buf = new Buffer(12)
-          buf.writeFloatBE(xyz[0], 0)
-          buf.writeFloatBE(xyz[1], 4)
-          buf.writeFloatBE(xyz[2], 8)
-          return {
-            type: 'put',
-            key: String(item.id),
-            value: buf
-          }
-        }
-      }
     }
-  })
+    function writeWay (items, enc, next) {
+      var pending = 0
+      items.forEach(function (item) {
+        if (mode === RELATIONS) return
+        if (item.type === 'relation') {
+          mode = RELATIONS
+          // todo: relations
+          return
+        }
+        if (item.type !== 'way') {
+          return next(new Error('unexpected non-way type'))
+        }
+
+        pending++
+        var nodes = [], bufs = []
+        var ipending = item.refs.length
+        item.refs.forEach(function (ref, ix) {
+          db.get(String(ref), function (err, buf) {
+            if (err) console.error(err)
+            var xyz = [
+              buf.readFloatBE(0),
+              buf.readFloatBE(4),
+              buf.readFloatBE(8)
+            ]
+            nodes[ix] = xyz
+            bufs[ix] = buf
+            if (--ipending === 0) done()
+          })
+        })
+
+        function done () {
+          nodes.forEach(function (node, i) {
+            // key: [current xyz]
+            // value: [prev xyz] [next xyz]
+            var value = new Buffer(24)
+            bufs[(i+nodes.length-1)%nodes.length].copy(value)
+            bufs[(i+1)%nodes.length].copy(value, 12)
+            kdb.insert(node, value, function (err) {
+              if (err) console.error(err)
+              if (--pending === 0) next()
+            })
+          })
+        }
+      })
+      if (pending === 0) next()
+    }
+  }
 }
 
-function pipeline () {
-  var b = new OSM.BlobParser
-  var d = new OSM.BlobDecompressor
-  var p = new OSM.PrimitivesParser
-  var stream = combine.obj([ b, through.obj(write), d, p ])
-  stream.blob = b
-  stream.decompressor = d
-  stream.primitives = p
-  stream.offsets = []
-  return stream
-
-  function write (chunk, enc, next) {
-    if (chunk.type === 'OSMData') {
-      stream.offsets.push(chunk.offset)
-    }
-    next(null, chunk)
+function nodeRecord (item) {
+  var xyz = ecef(item.lat, item.lon, 0)
+  var buf = new Buffer(12)
+  buf.writeFloatBE(xyz[0], 0)
+  buf.writeFloatBE(xyz[1], 4)
+  buf.writeFloatBE(xyz[2], 8)
+  return {
+    type: 'put',
+    key: String(item.id),
+    value: buf
   }
 }
