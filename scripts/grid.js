3e3f30eb5be2de6525ab1c6b45960d6b4dd32ebd
James Halliday <mail@substack.net>
2016-07-17 16:10:13 -0700

    ymin,ymax work

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,4 +1,8 @@
-module.exports = function (opts) {
+var PI = Math.PI
+var cos = Math.cos, acos = Math.acos
+var sin = Math.sin, asin = Math.asin
+
+module.exports = function compute (opts) {
   var nx = opts.xcount
   var ny = opts.ycount
   var xmin = opts.xmin
@@ -7,23 +11,21 @@ module.exports = function (opts) {
   var ymax = opts.ymax
   var xspan = xmax - xmin
   var yspan = ymax - ymin
+  var zmin = sin(ymin*PI/180)
+  var zmax = sin(ymax*PI/180)
 
   var grid = []
   for (var x = 0; x < nx; x++) {
-    var x0 = x/nx*360-180
-    var x1 = (x+1)/nx*360-180
+    var x0 = xmin + x/nx*xspan
+    var x1 = xmin + (x+1)/nx*xspan
     for (var y = 0; y < ny; y++) {
-      var p0 = (y/ny*2-1)
-      var p1 = ((y+1)/ny*2-1)
-      var y0 = Math.asin(p0) / Math.PI * 180
-      var y1 = Math.asin(p1) / Math.PI * 180
-      grid.push({
-        xmin: x0,
-        xmax: x1,
-        ymin: y0,
-        ymax: y1
-      })
+      var p0 = f(y)
+      var p1 = f(y+1)
+      var y0 = asin(p0) / PI * 180
+      var y1 = asin(p1) / PI * 180
+      grid.push({ xmin: x0, xmax: x1, ymin: y0, ymax: y1 })
     }
   }
   return grid
+  function f (y) { return y/ny*(zmax-zmin)+zmin }
 }
