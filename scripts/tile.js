fb939d4de909c3dc9da213b90c0eecf20999db1a
James Halliday <mail@substack.net>
2016-07-17 18:13:09 -0700

    modularize tile creation, parallelize with nproc

--- scripts/tile.js
+++ scripts/tile.js
@@ -1,64 +1,70 @@
-#!/bin/bash
+#!/usr/bin/env node
 
 var path = require('path')
 var spawn = require('child_process').spawn
 var fs = require('fs')
 
-var minimist = require('minimist')
-var argv = minimist(process.argv.slice(2), {
-  alias: { i: 'infile', o: 'outdir' }
-})
-
-var grid = require('./grid.js')({
-  xcount: argv.xcount,
-  ycount: argv.ycount,
-  xmin: argv.xmin,
-  xmax: argv.xmax,
-  ymin: argv.ymin,
-  ymax: argv.ymax
-})
-
-var meta = {}
-grid.forEach(function (g, i) {
-  meta[i] = [ g.xmin, g.ymin, g.xmax, g.ymax ]
-})
-var metastr = JSON.stringify(meta)
-fs.writeFile(path.join(argv.outdir, 'meta.json'), metastr, function (err) {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-})
-
-;(function next (i) {
-  if (i >= grid.length) return
-  var g = grid[i]
-  var wsen = [ g.xmin, g.ymin, g.xmax, g.ymax ]
-  var cmd = [
-    'osmconvert',
-    argv.infile,
-    '-b=' + wsen.join(','),
-    '--out-o5m'
-  ]
-  console.log(cmd.join(' '))
-  var ps = {
-    osmconvert: spawn(cmd[0], cmd.slice(1)),
-    gzip: gzip()
-  }
-  ps.osmconvert.stderr.pipe(process.stderr)
-
-  var outfile = path.join(argv.outdir, i + '.o5m.gz')
-  ps.osmconvert.stdout.pipe(ps.gzip.stdin)
-  ps.gzip.stdout.pipe(fs.createWriteStream(outfile))
-
-  ps.osmconvert.once('close', function (code) {
-    if (code !== 0) process.exit(code)
+module.exports = function (opts, cb) {
+  var grid = require('./grid.js')({
+    xcount: opts.xcount,
+    ycount: opts.ycount,
+    xmin: opts.xmin,
+    xmax: opts.xmax,
+    ymin: opts.ymin,
+    ymax: opts.ymax
   })
-  ps.gzip.once('close', function (code) {
-    if (code !== 0) process.exit(code)
-    else next(i+1)
+  var meta = {}
+  grid.forEach(function (g, i) {
+    meta[i] = [ g.xmin, g.ymin, g.xmax, g.ymax ]
   })
-})(0)
+  var metastr = JSON.stringify(meta)
+  fs.writeFile(path.join(opts.outdir, 'meta.json'), metastr, function (err) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+  })
+
+  var nproc = opts.nproc || 1
+  var offsets = []
+  for (var n = 0; n < nproc; n++) {
+    var i = Math.floor(n*grid.length/nproc)
+    var j = Math.floor((n+1)*grid.length/nproc)
+    offsets.push([i,j])
+  }
+
+  offsets.forEach(function (range) {
+    (function next (i, j) {
+      if (i >= j) return
+      var g = grid[i]
+      var wsen = [ g.xmin, g.ymin, g.xmax, g.ymax ]
+      var cmd = [
+        'osmconvert',
+        opts.infile,
+        '-b=' + wsen.join(','),
+        '--out-o5m'
+      ]
+      console.log(cmd.join(' '))
+      var ps = {
+        osmconvert: spawn(cmd[0], cmd.slice(1)),
+        gzip: gzip()
+      }
+      ps.osmconvert.stderr.pipe(process.stderr)
+
+      var outfile = path.join(opts.outdir, i + '.o5m.gz')
+      ps.osmconvert.stdout.pipe(ps.gzip.stdin)
+      ps.gzip.stdout.pipe(fs.createWriteStream(outfile))
+
+      ps.osmconvert.once('close', function (code) {
+        if (code !== 0) process.exit(code)
+      })
+      ps.gzip.once('close', function (code) {
+        if (code !== 0) process.exit(code)
+        else next(i+1, j)
+      })
+    })(range[0], range[1])
+  })
+}
 
 function gzip () {
   var ps = spawn('gzip')
