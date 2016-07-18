93b0ee84baf8ff3b11f2b73783becd642e31a6e0
James Halliday <mail@substack.net>
2016-07-17 17:42:39 -0700

    write a meta file

--- scripts/tile.js
+++ scripts/tile.js
@@ -2,6 +2,7 @@
 
 var path = require('path')
 var spawn = require('child_process').spawn
+var fs = require('fs')
 
 var minimist = require('minimist')
 var argv = minimist(process.argv.slice(2), {
@@ -17,6 +18,18 @@ var grid = require('./grid.js')({
   ymax: argv.ymax
 })
 
+var meta = {}
+grid.forEach(function (g, i) {
+  meta[i] = [ g.xmin, g.ymin, g.xmax, g.ymax ]
+})
+var metastr = JSON.stringify(meta)
+fs.writeFile(path.join(argv.outdir, 'meta.json'), metastr, function (err) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+})
+
 ;(function next (i) {
   if (i >= grid.length) return
   var g = grid[i]
@@ -25,14 +38,30 @@ var grid = require('./grid.js')({
     'osmconvert',
     argv.infile,
     '-b=' + wsen.join(','),
-    '--out-o5m',
-    '-o=' + path.join(argv.outdir, i + '.o5m')
+    '--out-o5m'
   ]
   console.log(cmd.join(' '))
-  var ps = spawn(cmd[0], cmd.slice(1))
-  ps.stderr.pipe(process.stderr)
-  ps.once('exit', function (code) {
+  var ps = {
+    osmconvert: spawn(cmd[0], cmd.slice(1)),
+    gzip: gzip()
+  }
+  ps.osmconvert.stderr.pipe(process.stderr)
+
+  var outfile = path.join(argv.outdir, i + '.o5m.gz')
+  ps.osmconvert.stdout.pipe(ps.gzip.stdin)
+  ps.gzip.stdout.pipe(fs.createWriteStream(outfile))
+
+  ps.osmconvert.once('close', function (code) {
+    if (code !== 0) process.exit(code)
+  })
+  ps.gzip.once('close', function (code) {
     if (code !== 0) process.exit(code)
     else next(i+1)
   })
 })(0)
+
+function gzip () {
+  var ps = spawn('gzip')
+  ps.gzip.stderr.pipe(process.stderr)
+  return ps
+}
